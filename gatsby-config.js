073196407cf5cd78065955553a7154a037c861d6
Oleksii Prokhorenko <oleksii@stellaruk.co.uk>
2023-11-08 13:26:18 +0200

    lab2

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+{
+    resolve: `gatsby-plugin-s3`,
+    options: {
+        bucketName: "cfst-3457-d0f7bded10d329b1f1e38a4c5d49-prodbucket-1pkxh80wvz314",
+        protocol: "https",
+        hostname: "cfst-3457-d0f7bded10d329b1f1e38a4c5d49-prodbucket-1pkxh80wvz314.s3-website-us-east-1.amazonaws.com",
+    },
+}
